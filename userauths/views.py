eeca206850a684aa441a2685b5c9dbbdef4bb20d
Marcelo Galli <marcelolemesgalli@hotmail.com>
2024-05-11 13:32:59 -0300

    update
    update

--- userauths/views.py
+++ userauths/views.py
@@ -1,34 +1,67 @@
-from django.shortcuts import redirect, render
-from userauths.forms import UserRegistrationForm
-from django.contrib.auth import authenticate, login
+# pylint: disable=all
+# flake8: noqa
+
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
+
+from userauths.models import User
+from userauths.forms import UserRegisterForm
 
 
 def RegisterView(request):
-    if request.method == 'POST':
-        # se eu nao passar o request.POST ele nao vai capturar os valores do formulario
-        form = UserRegistrationForm(request.POST)
+    if request.user.is_authenticated:
+        messages.warning(request, "You are already logged in.")
+        return redirect("account:account")
+
+    if request.method == "POST":
+        form = UserRegisterForm(request.POST)
         if form.is_valid():
             # form.save()
-            new_user = form.save()
-            username = form.cleaned_data.get('username')
-            # username = request.POST.get('username') isso aqui é a mesma coisa que a linha acima
-            messages.success(request, f"Hey {username} was account is successfully!")
-            new_user = authenticate(
-                username=form.cleaned_data.get('email'),
-                password=form.cleaned_data.get('password1')
-            )
+            new_user = form.save() # new_user.email
+            username = form.cleaned_data.get("username")
+            # username = request.POST.get("username")
+            messages.success(request, f"Hey {username}, your account was created successfully.")
+            # new_user = authenticate(username=form.cleaned_data.get('email'))
+            new_user = authenticate(username=form.cleaned_data['email'],
+                                    password=form.cleaned_data['password1'])
             login(request, new_user)
-            return redirect('core:index')
-        
-    if request.user.is_authenticated:
-        messages.warning(request, f"You are already logged in!")
-        return redirect('core:login')
-    else:
-        form = UserRegistrationForm()
+            return redirect("account:account")
 
+    else:
+        form = UserRegisterForm()
     context = {
-        # passamos o formulario aqui no context, isso faz com que gere o formulario de usuario renderizado na pagina
-        "form": form,
+        "form": form
     }
-    return render(request, 'userauths/sign-up.html', context)
+    return render(request, "userauths/sign-up.html", context)
+
+
+def LoginView(request):
+    if request.method == "POST":
+        email = request.POST.get("email")
+        password = request.POST.get("password")
+
+        try:
+            user = User.objects.get(email=email)
+            user = authenticate(request, email=email, password=password)
+
+            if user is not None: # if there is a user
+                login(request, user)
+                messages.success(request, "You are logged.")
+                return redirect("account:account")
+            else:
+                messages.warning(request, "Username or password does not exist")
+                return redirect("userauths:sign-in")
+        except:
+            messages.warning(request, "User does not exist")
+
+    if request.user.is_authenticated:
+        messages.warning(request, "You are already logged In")
+        return redirect("account:account")
+        
+    return render(request, "userauths/sign-in.html")
+
+def logoutView(request):
+    logout(request)
+    messages.success(request, "You have been logged out.")
+    return redirect("userauths:sign-in")
