206dd4a9a448864d1e4db52d10e6c1ab6502c937
Marcelo Galli <marcelolemesgalli@hotmail.com>
2024-05-10 22:55:31 -0300

    update
    update

--- core/views.py
+++ core/views.py
@@ -2,5 +2,5 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 
-def index(request, *args, **kwargs):
+def index(request):
     return render(request, 'core/index.html')
--- userauths/views.py
+++ userauths/views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 from userauths.forms import UserRegistrationForm
+from django.contrib.auth import authenticate, login
+from django.contrib import messages
 
 
 def RegisterView(request):
@@ -7,7 +9,21 @@ def RegisterView(request):
         # se eu nao passar o request.POST ele nao vai capturar os valores do formulario
         form = UserRegistrationForm(request.POST)
         if form.is_valid():
-            form.save()
+            # form.save()
+            new_user = form.save()
+            username = form.cleaned_data.get('username')
+            # username = request.POST.get('username') isso aqui é a mesma coisa que a linha acima
+            messages.success(request, f"Hey {username} was account is successfully!")
+            new_user = authenticate(
+                username=form.cleaned_data.get('email'),
+                password=form.cleaned_data.get('password1')
+            )
+            login(request, new_user)
+            return redirect('core:index')
+        
+    if request.user.is_authenticated:
+        messages.warning(request, f"You are already logged in!")
+        return redirect('core:login')
     else:
         form = UserRegistrationForm()
 
@@ -15,4 +31,4 @@ def RegisterView(request):
         # passamos o formulario aqui no context, isso faz com que gere o formulario de usuario renderizado na pagina
         "form": form,
     }
-    return render(request, 'userauths/sign-up.html', context)
+    return render(request, 'userauths/sign_up.html', context)
