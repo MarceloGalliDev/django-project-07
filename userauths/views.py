c135b91f084d651519a2de6f75a779150ebcd897
Marcelo Galli <marcelolemesgalli@hotmail.com>
2024-05-10 21:30:05 -0300

    update
    update

--- userauths/views.py
+++ userauths/views.py
@@ -1,5 +1,18 @@
 from django.shortcuts import render
+from userauths.forms import UserRegistrationForm
 
 
 def RegisterView(request):
-    return render(request, 'userauths/sign-up.html')
+    if request.method == 'POST':
+        # se eu nao passar o request.POST ele nao vai capturar os valores do formulario
+        form = UserRegistrationForm(request.POST)
+        if form.is_valid():
+            form.save()
+    else:
+        form = UserRegistrationForm()
+
+    context = {
+        # passamos o formulario aqui no context, isso faz com que gere o formulario de usuario renderizado na pagina
+        "form": form,
+    }
+    return render(request, 'userauths/sign-up.html', context)
